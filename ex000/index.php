175802b7f314a26f55ee2f23d9c5f2fe83bb3883
Gabriel Erick <gabrielmmve13@gmail.com>
2025-11-03 12:56:50 -0300

    modulo-1

---
+++ ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da Maldição</title>
+</head>
+
+<body>
+
+    <h1>
+        <!-- \u{1F30D} 1F30D É UM CODEPOINT PARA O EMOJI MUNDO -->
+        <?php echo "Olá mundo! \u{1F30D}"; ?>
+    </h1>
+
+    <p>Vamos tentar nos livrar da maldição</p>
+
+</body>
+
+</html>
---
+++ ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Exemplo de PHP</h1>
+
+    <?php 
+    // mudando o fuso horário para São Paulo (que estava configurado no servidor como UTC)
+    date_default_timezone_set("America/Sao_Paulo");
+    echo "Hoje é dia " . date("d/M/Y") . "!" . PHP_EOL;
+    echo "e a hora atual é " . date("G:i:s") . "!";
+    ?>
+</body>
+</html>
